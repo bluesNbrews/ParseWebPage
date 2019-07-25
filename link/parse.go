78f8dd76409ce24d9f6aa61095201e4349302ade
Steven <mstevenwilliams@gmail.com>
2019-07-25 15:55:17 -0400

    Traverse user defined HTML (statically created) using a Depth First Search (DFS) and print to console.

--- link/parse.go
+++ link/parse.go
@@ -1,6 +1,12 @@
 package link
 
-import "io"
+import (
+	"io"
+	"fmt"
+
+	"golang.org/x/net/html"
+)
+
 
 //This represents an 'a' html tag with href (URL)
 type Link struct {
@@ -10,5 +16,21 @@ type Link struct {
 
 //Parse will take in an HTML document and will return a slice of links parsed from it.
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string){
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(padding, msg)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
 }
