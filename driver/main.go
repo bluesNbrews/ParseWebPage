13fd9fd4e0cc742aa79d7546e127d348ad4006f7
Steven <mstevenwilliams@gmail.com>
2019-07-26 15:02:22 -0400

    Test dummy data (HTML).

--- driver/main.go
+++ driver/main.go
@@ -11,9 +11,7 @@ import (
 var exampleHTML = `
 <html>
 <body>
-  <h1>Hello!</h1>
-  <a href="/other-page">A link to another page</a>
-  <a href="/page-two">A link to a second page</a>
+  <a href="/dog-cat">dog cat <!-- commented text SHOULD NOT be included! --></a>
 </body>
 </html>
 `
