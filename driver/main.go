5e078df1829bc97a9f3c0120951d350e011cd228
Fadi Dagher <fdagher1@gmail.com>
2019-07-27 22:00:54 -0400

    Added code to retrieve HTML content from a live URL

--- driver/main.go
+++ driver/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
 	"strings"
 
 	"github.com/bluesNbrews/ParseWebPage/link"
@@ -17,14 +21,36 @@ var exampleHTML = `
 `
 
 func main() {
-	
-	r := strings.NewReader(exampleHTML)
-	
-	links, err := link.Parse(r)
-	
+
+	//Retrieve URL to query
+	var fullpath = string(os.Args[1])
+
+	//Retrieve domain name from URL (to be used later for appending to URL paths)
+	zp := regexp.MustCompile(`/`)
+	var temp = zp.Split(fullpath, -1)
+	var domainname = temp[0] + "//" + temp[2]
+
+	//Make GET request to the entered URL and store HTML in variable r
+	resp, err := http.Get(fullpath)
 	if err != nil {
 		panic(err)
 	}
-	
-	fmt.Printf("%+v\n", links)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	r := bytes.NewReader(body)
+
+	//Parse will take in an HTML document (r) and will return a slice of links parsed from it.
+	links, err := link.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(links); i++ {
+		var path = links[i].Href
+		if strings.HasPrefix(path, "/") {
+			path = domainname + path
+		}
+		fmt.Println(path)
+	}
+
 }
--- link/parse.go
+++ link/parse.go
@@ -1,34 +1,32 @@
 package link
 
 import (
-	
 	"io"
 	"strings"
+
 	"golang.org/x/net/html"
-
 )
-
 
 //This represents an 'a' html tag with href (URL)
 type Link struct {
-	
 	Href string
 	Text string
 }
 
 //Parse will take in an HTML document and will return a slice of links parsed from it.
 func Parse(r io.Reader) ([]Link, error) {
-	
+
 	doc, err := html.Parse(r)
-	
+
 	if err != nil {
 		return nil, err
 	}
-	
+
+	//linkNodes Takes an HTML body and returns an array of HTML elements containing the <a> tag
 	nodes := linkNodes(doc)
 
 	var links []Link
-	
+
 	for _, node := range nodes {
 		links = append(links, buildLink(node))
 		//fmt.Println(node)
@@ -36,31 +34,35 @@ func Parse(r io.Reader) ([]Link, error) {
 	return links, nil
 }
 
+//buildLink takes an html node and converts it to type link
 func buildLink(n *html.Node) Link {
-	
+
 	var ret Link
 
 	for _, attr := range n.Attr {
-		
+
 		if attr.Key == "href" {
 			ret.Href = attr.Val
 			break
 		}
 	}
+
+	//text takes an html node and returns the text in it
 	ret.Text = text(n)
 	return ret
 }
 
+//text takes an html node and returns the text in it
 func text(n *html.Node) string {
 	if n.Type == html.TextNode {
 		return n.Data
 	}
-	if n.Type != html.ElementNode{
+	if n.Type != html.ElementNode {
 		return ""
 	}
-	
-	var ret string 
-	
+
+	var ret string
+
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		//Byte buffer?
 		ret += text(c) + " "
@@ -71,15 +73,16 @@ func text(n *html.Node) string {
 	return ret
 }
 
+//Takes an HTML body and returns array of HTML elements containing the <a> tag
 func linkNodes(n *html.Node) []*html.Node {
-	
+
 	if n.Type == html.ElementNode && n.Data == "a" {
 		return []*html.Node{n}
 	}
 
 	var ret []*html.Node
 
-	for c := n.FirstChild; c!= nil; c = c.NextSibling {
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		ret = append(ret, linkNodes(c)...)
 	}
 	return ret
