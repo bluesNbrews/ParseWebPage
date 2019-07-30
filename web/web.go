4ec02e52ba2cba9662be7a7682b897be7efc68b3
Steven <mstevenwilliams@gmail.com>
2019-07-29 23:49:01 -0400

    Fixing error 'Get : unsupported protocol scheme ""'. Aka empty hrefs.

--- web/web.go
+++ web/web.go
@@ -26,7 +26,7 @@ func Gethtml(enteredurl string) io.Reader {
 	return htmlcontent
 }
 
-/*//Getandprinturlstatus makes a GET call to the passed URLs and prints status for each
+//Getandprinturlstatus makes a GET call to the passed URLs and prints status for each
 func Getandprinturlstatus(newlinks []link.Link) {
 
 	//Print table header
@@ -41,36 +41,7 @@ func Getandprinturlstatus(newlinks []link.Link) {
 		//If item not previously checked, then get HTTP return code, else set return code to (prev. checked)
 		var returncode string
 
-		if !(prevchecked) {
-			resp, err := http.Get(newlinks[i].Href)
-			if err != nil {
-				log.Fatal(err)
-			}
-			returncode = strconv.Itoa(resp.StatusCode)
-		} else {
-			returncode = "(prev. checked)"
-		}
-
-		//Print table rows
-		fmt.Printf("%-3s| %-60s | %-30s | %-16s|\n", strconv.Itoa(i+1), newlinks[i].Href, newlinks[i].Text, returncode)
-	}
-}*/
-
-func Getandprinturlstatus(newlinks []link.Link) {
-
-	//Print table header
-	fmt.Printf("%-3s| %-60s | %-30s | %-16s|\n", "#", "URL LINKS", "URL TEXT", "CODE")
-
-	//Iterate over each item in newlinks array, if not previously check then get HTTP code, and display output
-	for i := 0; i < len(newlinks); i++ {
-
-		//Check if item was previously checked
-		prevchecked := previouslychecked(newlinks[i].Href, newlinks, i)
-
-		//If item not previously checked, then get HTTP return code, else set return code to (prev. checked)
-		var returncode string
-
-		if !(prevchecked) {
+		if !(prevchecked) && newlinks[i].Href != ""{
 			resp, err := http.Get(newlinks[i].Href)
 			if err != nil {
 				log.Fatal(err)
